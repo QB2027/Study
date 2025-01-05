bb760ed18b28dd9978f615232dfeee416eec2862
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2025-01-05 23:01:06 +0800

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
 from utils.scanner import scan_directory
 from utils.json_utils import save_to_json
-import os
 
 def main():
     """
     主函数，负责调用模块化功能完成任务。
     """
-    # 仓库根目录，修复重复路径问题
+    # 仓库根目录，优先从环境变量 GITHUB_WORKSPACE 获取
     repo_root = os.getenv("GITHUB_WORKSPACE", os.getcwd())
 
     # 输出文件路径
